8315214fad12761b4c459768cf6a118fb808210f
kishan singh <kishan.singh@mountblue.io>
2019-03-21 11:38:01 +0530

    everything is fine

--- ipl_front/app.js
+++ ipl_front/app.js
@@ -2,7 +2,7 @@ fetch("data.json").then(function (data) {
     return data.json();
 }).then(function (myjson) {
     // console.log(myjson[Object.keys(myjson)[3]]);
-   
+
     let mykeys = Object.keys(Object.values(myjson)[0]);
     let temp = [];
     for (let i = 0; i < mykeys.length; i++) {
@@ -12,28 +12,48 @@ fetch("data.json").then(function (data) {
         temp.push(obj);
     }
     visualize(temp);
-    console.log(Object.values(myjson)[1][2008]["Kolkata Knight Riders"]);
+    //console.log(Object.values(myjson)[1][2008]["Kolkata Knight Riders"]);
+
     //-----------------------------------------------
-    let teams = Object.keys(Object.values(myjson)[1][2008]);
-    mykeys = Object.keys(Object.values(myjson)[1]);
-    // console.log(mykeys);
+
+     mykeys = Object.keys(Object.values(myjson)[1]);
+      // console.log(mykeys);
+     let teams = [];
+     for (let i = 0; i < mykeys.length; i++) {
+         teams.push(Object.keys(Object.values(myjson)[1][mykeys[i]]));
+     }
+     //console.log(teams);
+    let myteam = teams.flat();
+    let teamsno = [...new Set(myteam)];
+    console.log(teamsno);
+
     let mytemp = [];
-   for (let i = 0; i < teams.length; i++) {
+    for (let i = 0; i < teamsno.length; i++) {
         obj = {};
         temp = [];
-        obj["name"] = teams[i];
-       for(let j=0;j<mykeys.length;j++) {
-        let val = Object.values(myjson)[1][mykeys[j]][teams[i]];
-        temp.push(val);
-       }
-      obj['data'] = temp;
-      mytemp.push(obj);
-   }
-  
-   visualize1(mytemp,mykeys);
+        obj["name"] = teamsno[i];
+        for (let j = 0; j < mykeys.length; j++) {
+            let val;
+            if(!isNaN(Object.values(myjson)[1][mykeys[j]][teamsno[i]])){
+                 val = Object.values(myjson)[1][mykeys[j]][teamsno[i]];
+            } else{
+                val = 0;
+            }
+            
+            temp.push(val);
+        }
+        obj['data'] = temp;
+        mytemp.push(obj);
+    }
+
+    visualize1(mytemp, mykeys);
+
+
+
+
     //-----------------------------------------------
-     mykeys = Object.keys(Object.values(myjson)[2]);
-     temp = [];
+    mykeys = Object.keys(Object.values(myjson)[2]);
+    temp = [];
     for (let i = 0; i < mykeys.length; i++) {
         obj = {};
         obj["name"] = mykeys[i];
@@ -41,9 +61,14 @@ fetch("data.json").then(function (data) {
         temp.push(obj);
     }
     visualize2(temp);
+
+
+
+
+
     //----------------------------------------------
-     mykeys = Object.keys(Object.values(myjson)[3]);
-     temp = [];
+    mykeys = Object.keys(Object.values(myjson)[3]);
+    temp = [];
     for (let i = 0; i < mykeys.length; i++) {
         obj = {};
         obj["name"] = mykeys[i];
@@ -59,14 +84,14 @@ const visualize = (data) => {
             type: 'column'
         },
         title: {
-            text: 'Total number of matches played'
+            text: 'Number of matches played per year for all the years in IPL'
         },
         xAxis: {
             type: 'category'
         },
         yAxis: {
             title: {
-                text: 'Total number of matches'
+                text: 'Number of matches played per year'
             }
         },
         "series": [{
@@ -78,13 +103,13 @@ const visualize = (data) => {
 
 }
 
-const visualize1 = (mytemp,mykeys) => {
+const visualize1 = (mytemp, mykeys) => {
     Highcharts.chart('container1', {
         chart: {
             type: 'bar'
         },
         title: {
-            text: 'Stacked bar chart'
+            text: 'Number of matches won of per team per year in IPL'
         },
         xAxis: {
             categories: mykeys
@@ -92,14 +117,14 @@ const visualize1 = (mytemp,mykeys) => {
         yAxis: {
             min: 1,
             title: {
-                text: 'Total fruit consumption'
+                text: 'Number of matches won'
             }
         },
         plotOptions: {
             series: {
-              stacking: 'normal'
+                stacking: 'normal'
             }
-          },
+        },
         series: mytemp
     });
 
@@ -111,18 +136,18 @@ const visualize2 = (data) => {
             type: 'column'
         },
         title: {
-            text: 'Total number of matches played'
+            text: 'Extra runs conceded per team in 2016'
         },
         xAxis: {
             type: 'category'
         },
         yAxis: {
             title: {
-                text: 'Total number of matches'
+                text: 'Extra runs conceded per team'
             }
         },
         "series": [{
-            "name": "Years",
+            "name": "Extra_run",
             "colorByPoint": true,
             "data": data
         }],
@@ -136,18 +161,18 @@ const visualize3 = (data) => {
             type: 'column'
         },
         title: {
-            text: 'Total number of matches played'
+            text: 'Top 10 economical bowlers in 2015'
         },
         xAxis: {
             type: 'category'
         },
         yAxis: {
             title: {
-                text: 'Total number of matches'
+                text: 'Top 10 economical bowlers'
             }
         },
         "series": [{
-            "name": "Years",
+            "name": "per_Over",
             "colorByPoint": true,
             "data": data
         }],
